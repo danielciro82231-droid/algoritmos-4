756d7695772f19d4383265e844b3e4cfe0e85d85
danielciro82231-droide <daniel_ciro82231@elpoli.edu.co>
2026-02-05 20:25:57 -0500

    cambios 2

---
+++ practicalistas.py
@@ -0,0 +1,22 @@
+class nodo
+    def __init__(self, documento, nombre):
+        self.documento = documento
+        self.nombre = nombre
+        self.siguiente = None
+
+class lista:
+    def __init__(self):
+        self.cabeza = None
+
+    def agregarAlFinal(self, documento, nombre):
+       nodo_ Nodo(documento, nombre)
+
+       if self.cabeza == None:
+            self.cabeza == nodo
+            self.cola = nodo
+
+       else:
+          self.cola.siguiente = Nodo
+          self .cola = nodo
+
+               
