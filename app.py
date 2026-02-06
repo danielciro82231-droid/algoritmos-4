489dbe273ea472278ce907a78360b68041c3b68a
danielciro82231-droide <daniel_ciro82231@elpoli.edu.co>
2026-02-05 19:23:47 -0500

    hghfg

---
+++ app.py
@@ -0,0 +1,11 @@
+class Estudiante:
+    def __init__(self, nombre, documento):
+        self.nombre = nombre
+         self.documento = documento
+
+estudiante1 = Estudiante("Juan", 1111)
+print(estudiante1.__nombre)
+
+
+
+    
